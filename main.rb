1efa95b96e6ea4ee0717923fa6b9498abecce849
tushar994 <tkumar994@gmail.com>
2020-11-28 23:44:10 +0530

    finished the padding function

--- main.rb
+++ main.rb
@@ -1,1 +1,7 @@
-require_relative "math_functions.rb"
+require_relative "math_functions"
+require_relative "padding_converter"
+include Converter
+
+padded_array =  Converter.convert("abcdefghijklmonpqrstuvwxyzabcdefghijklmonpqrstuvwxyzabcdefghijklmonpqrstuvwxyzabcdefghijklmonpqrstuvwxyzabcdefghijklmonpqrstuvwxyzabcdefghijklmonpqrstuvwxyz")
+# print padded_array
+# print "\n"
--- padding_converter.rb
+++ padding_converter.rb
@@ -1,18 +1,40 @@
-module converter
+module Converter
     def convert(string)
         # we have to take the string, and return the padded array, where each element in the array has 32/64 bits
-        # we are modifying the algorithm, such that we get an array of size n*16*(64or32) bits/ n*16*size_of_word bytes
+        # we are modifying the algorithm, such that we get an array of size n*16*(64) bits/ n*16*size_of_word bytes
         # now, we have a certain number of characters that contain 8 bits/ 1 byte each. 
         # we will use ord to get asci number of characters.
         size_of_word = 0.size
-        # if it's 8 then 64 bits is one word, otherwise it's 32?
-        size_of_string = string.length
-        size_of_string += 64/size_of_word
-        no_of_size_of_words = (size_of_string*8)/size_of_word
-        no_of_size_of_words += no_of_size_of_words%16
+        size_of_string = string.length/size_of_word
+        if string.length%size_of_word!=0
+            size_of_string+=1
+        end
+        size_of_string += 1
+        no_of_words = size_of_string
+        puts "no_of_words is " + no_of_words.to_s
         # now, no_of_size_of_words is the number of words that we need to store padded string
         final_array = []
-
-
+        integer = 0
+        for i in 0..string.length-1 do
+            # puts "i is init"+ i.to_s
+            integer += string[i].ord
+            if i%size_of_word != size_of_word-1
+                integer*= 2**(8)
+            else
+                # puts integer
+                final_array.push(integer)
+                integer=0
+            end
+        end
+        if (string.length-1)%size_of_word!=size_of_word-1 
+            final_array.push(integer)
+        end
+        final_array.push(string.length)
+        for i in 1..(16-(no_of_words%16)) do
+            # puts "i is "+ i.to_s
+            final_array.insert(-2,0)
+        end
+        puts "length of it is " + final_array.length.to_s
+        return final_array
     end
 end
